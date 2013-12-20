7e10483743b7f6e528f8deb224bfb19cc665480c
Jeremy Walton <jthelegoman@gmail.com>
2013-12-20 10:20:35 -0500

    Got feedback and fixed one more bug

--- Code/Go Fish/round_results.rb
+++ Code/Go Fish/round_results.rb
@@ -12,8 +12,8 @@ class RoundResults
 		message = "#{@asker.name} asked #{@giver.name} for #{@card_rank}s, "
 		message2 = "but he/she had none, #{@asker.name} will take from deck. "
 		message3 = "and he/she had #{@number_of_cards}, #{@asker.name} took them. "
-		message4 = "#{@asker.name} found no matches"
-		message5 = "#{@asker.name} found these matches: #{@books}"
+		message4 = "#{@asker.name} has no matches"
+		message5 = "#{@asker.name} has these matches: #{@books}"
 		if(@number_of_cards == 0)
 			if (@books.first == nil)
 				return message + message2 + message4
@@ -27,7 +27,7 @@ class RoundResults
 				return message + message3 + message5
 			end
 		end
-		@books = []
+		#@books = []
 	end
 
 end	
